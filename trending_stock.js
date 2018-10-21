f9f5c317612de96e21f3da5a5ab68804aebf6d74
Sam Gura <sggura@buffalo.edu>
2018-10-21 14:08:11 -0400

    Revert "Changed prices to show prices to 2 decimal places instead of 5"
    This reverts commit 3fa75fa71d17f6fd3352ff27817a3dd3e4e35870.

--- trending_stock.js
+++ trending_stock.js
@@ -4,13 +4,13 @@ xml.open ("GET","https://api.iextrading.com/1.0/stock/market/list/gainers?displa
 var tableTxt = "";
 xml.onreadystatechange = function(){
 	if (xml.readyState === 4 && xml.status === 200) {
-		var jsonObj = JSON.parse(xml.responseText);
+		var jsonObj = JSON.parse(xml.responseText)
 		tableTxt += "<table><thead><tr class=table100-head><th class=column1>Name</th><th class=column2>Price (USD)</th><th class=column3>Percent Change (24hrs)</th><th class=column6>Percent Change (YTD)</th></tr></thead><tbody>";
 		for (var i=0; i<5; i++){
 			var sym = jsonObj[i].symbol;
 			var cos = jsonObj[i].latestPrice;
-			var percent24h = jsonObj[i].changePercent;
-			var yeartochange = jsonObj[i].ytdChange;
+			var percent24h = jsonObj[i].changePercent
+			var yeartochange = jsonObj[i].ytdChange
 			var cos_ = cos.toFixed(2);
 			var percent24h_ = percent24h.toFixed(2);
 			var yeartochange_ = yeartochange.toFixed(2);
