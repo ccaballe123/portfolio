facbf070318ad8671aa5a1c2ebaeb4f275429c45
michael-langaman <langaman.michael@gmail.com>
2018-10-22 14:26:48 -0400

    Changed unit tests descriptions to explain what each test is checking.

--- tests/registerTests.js
+++ tests/registerTests.js
@@ -35,58 +35,58 @@ describe('Register Tests', function() {
     expect(errorResult).toBe(expected);
   }
 
-  it('Acceptance Test 1:', function() {
+  it('should check if user is redirected to home page after successful login', function() {
     initializeValues("regTest@email.com", "123456", "123456", "1000");
     browser.sleep(6000);
     expect(browser.getCurrentUrl()).toBe('https://portfolio-47669.firebaseapp.com/home.html');
   });
 
-  it('Acceptance Test 2:', function() {
+  it('should check if the user input an email address', function() {
     initializeValues("", "123456", "123456", "1000");
     errorTest("Please input an email address.");
   });
 
-  it('Acceptance Test 3:', function() {
+  it('should check if the user filled out the first password field', function() {
     initializeValues("email@email.com", "", "123456", "1000");
     errorTest("Please fill out the first password field.");
   });
 
-  it('Acceptance Test 4:', function() {
+  it('should check if user filled out the confirm password field', function() {
     initializeValues("email@email.com", "123456", "", "1000");
     errorTest("Please fill out the second password field.");
   });
 
-  it('Acceptance Test 5:', function() {
+  it('should check if user input a desired balance', function() {
     initializeValues("email@email.com", "123456", "123456", "");
     errorTest("Please input a correct balance.")
   });
 
-  it('Acceptance Test 6:', function() {
+  it('should check if the user input a valid email address', function() {
     initializeValues("INVALID EMAIL", "123456", "123456", "1000");
     errorTest("The email address is badly formatted.");
   });
 
-  it('Acceptance Test 7:', function() {
+  it('should check if the user input identical passwords', function() {
     initializeValues("DIFFERENTPASSWORDS@email.com", "654321", "123456", "1000");
     errorTest("Both passwords must be identical.");
   });
 
-  it('Acceptance Test 8:', function() {
+  it('should check if the user input a password with a length greater than 6', function() {
     initializeValues("WEAKPASSWORD@email.com", "12345", "12345", "1000");
     errorTest("Password should be at least 6 characters");
   });
 
-  it('Acceptance Test 9:', function() {
+  it('should check if the user input a positive value for their desired balance', function() {
     initializeValues("NEGATIVEBALANCE@email.com", "123456", "123456", "-1000");
     errorTest("Please input a balance greater than zero.");
   });
 
-  it('Acceptance Test 10:', function() {
+  it('should check if the user input a valid value for the desired balance. (ex: any number with a dash in between) ', function() {
     initializeValues("INVALIDBALANCE@email.com", "123456", "123456", "123-456-7890");
     errorTest("Please input a correct balance.");
   });
 
-  it('Acceptance Test 11:', function() {
+  it('should check if the user can go back to the login page if they happen to have an account already', function() {
     regBtn = element(by.id('register_btn'));
     regBtn.click();
 
