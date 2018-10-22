64fb7a5b06034a250b88a5429633d1c64ecf1e47
kylespiv <kylespiv@buffalo.edu>
2018-10-22 15:49:58 -0400

    Updated python script to rewrite the table.html file with updated stock information

--- iexBatchTest.py
+++ iexBatchTest.py
@@ -19,8 +19,63 @@ def getStockInformation(json, symbols, type):
 
     return names
 
+def writeTable(json,symbols):
+    tableBody = 0
+    tr = -1
+    td = 0
+
+    names =  getStockInformation(json,symbols,"companyName")
+    prices = getStockInformation(json,symbols,"latestPrice")
+    change = getStockInformation(json,symbols,"changePercent")
+
+    htmlLineEnd = "</td>\n"
+
+    outputFile = open("Table/Table_Responsive_v1/table.html",'w')
+    inputFile =  open("Table/Table_Responsive_v1/table_copy.html","r")
+
+    lines = inputFile.readlines()
+    for line in lines:
+        #print(line)
+        if("<tbody>" in line):
+            tableBody = 1
+        if(tableBody == 1):
+            if("<tr>" in line):
+                #print("tr: " + str(tr))
+                tr = tr + 1
+                #print("tr: " + str(tr))
+                td = 0
+            if("<td " in line):
+                htmlLine = ""
+                if(td == 0):
+                    htmlLineStart = '         <td class="column1">'
+                    htmlLine = htmlLineStart + names[tr] + htmlLineEnd
+                elif(td == 1):
+                    htmlLineStart = '         <td class="column2">'
+                    htmlLine = htmlLineStart + symbols[tr].upper() + htmlLineEnd
+
+                elif(td == 2):
+                    htmlLineStart = '         <td class="column3">'
+                    htmlLine = htmlLineStart +  "$" + str(round(prices[tr],2)) + htmlLineEnd
+
+                elif(td == 3):
+                    htmlLineStart = '         <td class="column4">'
+                    htmlLine = htmlLineStart + str(round(change[tr]*100,2)) + "%" + htmlLineEnd
+
+                else:
+                    htmlLine = line
+                #print(htmlLine)
+                outputFile.write(htmlLine)
+                #print("td: " + str(td))
+                td = td + 1
+                #print("td: " + str(td))
+            else:
+                outputFile.write(line)
+        else:
+            outputFile.write(line)
+
+
 def main():
-    stockSymbols = ["fb","aapl","ge","tsla","hear"]
+    stockSymbols = ["fb","aapl","ge","tsla","lmt"]
     print(createGetRequest(stockSymbols))
     r = requests.get(createGetRequest(stockSymbols))
     r = r.json()
@@ -29,6 +84,7 @@ def main():
     print(getStockInformation(r,stockSymbols,"companyName"))
     print(getStockInformation(r,stockSymbols,"latestPrice"))
     print(getStockInformation(r,stockSymbols,"changePercent"))
+    writeTable(r,stockSymbols)
 
 if __name__ == '__main__':
     main()
