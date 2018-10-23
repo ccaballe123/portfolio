aa54874a843ce28a468b06c6f13fa495b95abdf1
kylespiv <kylespiv@buffalo.edu>
2018-10-22 21:46:07 -0400

    Finished javascript file to update table on stock portfolioo page

--- StockTable.js
+++ StockTable.js
@@ -1,12 +1,12 @@
-  var stockSymbols = ["LMT","FB","GOOGL","APPL","TSLA"]
+  var stockSymbols = ["AAPL","TSLA","LMT","GOOG","AMZN"];
 
   var xml = new XMLHttpRequest();
-  xml.open ("GET","https://api.iextrading.com/1.0/stock/market/batch?symbols=lmt,fb,goog,aapl,tsla&types=quote&range=1m&last=5", true);
+  xml.open ("GET","https://api.iextrading.com/1.0/stock/market/batch?symbols=aapl,tsla,lmt,goog,amzn&types=quote&range=1m&last=5", true);
 
   var tableTxt = "";
   xml.onreadystatechange = function()
   {
-    if(xml.readyState == 4 && xml.response === 200)
+    if(xml.readyState == 4 && xml.status === 200)
     {
       var jsonObj = JSON.parse(xml.responseText);
       tableTxt += "<table><thead><tr class=table100-head><th class=column1>Company Name</th><th class=column2>Symbol</th><th class=column3>Price</th><th class=column6>Percent Change</th></tr></thead><tbody>";
@@ -15,11 +15,11 @@
         var name          = jsonObj[stockSymbols[i]]["quote"]["companyName"];
         var symbol        = stockSymbols[i];
         var price         = jsonObj[stockSymbols[i]]["quote"]["latestPrice"].toFixed(2);
-        var percentChange = jsonObj[stockSymbols[i]]["quote"]["changePercent"].toFixed(2);
+        var percentChange = jsonObj[stockSymbols[i]]["quote"]["changePercent"];
 
-        tableTxt += "<tr><td class=column1>" + name +  "</td> <td class=column2> $" + symbol + "</td><td class=column3>" + price + " %</td><td class=column6>" + percentChange + " %</td></tr>";
+        tableTxt += "<tr><td class=column1>" + name +  "</td> <td class=column2>" + symbol + "</td><td class=column3>$" + price + "</td><td class=column6>" + (percentChange*100).toFixed(2) + " %</td></tr>";
       }
-      tableTxt += "</table>"
+      tableTxt += "</table>";
       document.getElementById("stock_table_data").innerHTML = tableTxt;
     }
   };
