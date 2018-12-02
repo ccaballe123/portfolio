88cd6a22bfc8bdc4ddd11ca00ead4f5b2eed5574
ccaballe123 <ccaballe@bufalo.edu>
2018-12-02 17:48:43 -0500

    Added onclick events to table rows that contain information on cryptocurrency to display more information. Information displayed includes a chart of a specific crypto's price action in the past month and the price of a specific crypto in 4 different regions.

--- crypto_portfolio.js
+++ crypto_portfolio.js
@@ -1,3 +1,46 @@
+function displayCryptoGraph(crypto){
+	var cid = crypto.cells[0].textContent;
+	baseUrl = "https://widgets.cryptocompare.com/";
+	(function (){
+		var appName = encodeURIComponent(window.location.hostname);
+		if(appName==""){appName="local";}
+		var s = document.createElement("script");
+		s.type = "text/javascript";
+		s.async = true;
+		var theUrl = baseUrl+'serve/v2/coin/chart?fsym='+cid+'&tsym=USD&period=1M';
+		s.src = theUrl + ( theUrl.indexOf("?") >= 0 ? "&" : "?") + "app=" + appName;
+		document.getElementById("crypto_graph").appendChild(s);
+	})();
+}
+
+function displayCryptoDetails(crypto){
+	var cid = crypto.cells[0].textContent;
+	baseUrl = "https://widgets.cryptocompare.com/";
+	(function (){
+		var appName = encodeURIComponent(window.location.hostname);
+		if(appName==""){appName="local";}
+		var s = document.createElement("script");
+		s.type = "text/javascript";
+		s.async = true;
+		var theUrl = baseUrl+'serve/v1/coin/header?fsym='+cid+'&tsyms=USD,EUR,GBP,CNY';
+		s.src = theUrl + ( theUrl.indexOf("?") >= 0 ? "&" : "?") + "app=" + appName;
+		document.getElementById('crypto_details').appendChild(s);
+	})();
+}
+
+function displayGraphAndDetailsOnRowClick(){
+	var numRows = document.getElementsByClassName("coins").length;
+	var i;
+	for(i = 0; i < numRows; i++){
+		document.getElementsByClassName("coins")[i].onclick = function() { 
+			document.getElementById("crypto_graph").innerHTML = '';
+			document.getElementById("crypto_details").innerHTML = '';
+			displayCryptoGraph(this); 
+			displayCryptoDetails(this);
+		 };
+	}
+}
+
 function populateCryptoTable(){
 	var map1 = new Map();
 	var currency = new Map();
@@ -15,25 +58,24 @@ function populateCryptoTable(){
 	var xml = new XMLHttpRequest();
 	xml.open("GET", "https://min-api.cryptocompare.com/data/pricemultifull?fsyms=" + cryptoString + "&tsyms=USD", true);
 
+	var tableText = "";
 	//Parse data from api and insert the data into table
 	xml.onreadystatechange = function () {
 		//Ensures api data is ready to be accessed
 		if (xml.readyState === 4 && xml.status === 200) {
 			var jsonData = JSON.parse(xml.responseText);
-			var index = 1;
-			var cryptoRowArray = [];
+			tableText += "<table id=\"crypto_table\"><tr><th>Name</th><th>Price Per Coin (USD)</th><th>Crypto Owned</th><th>Total Invested (USD)</th></tr>"
 		for (var [cryptoID, amountOwned] of firstCryptoPortfolio) {
-			cryptoRowArray[0] = cryptoID;
-			cryptoRowArray[1] = (jsonData.RAW[cryptoID].USD.PRICE).toFixed(2);	
-			cryptoRowArray[2] = amountOwned.toFixed(2);
-			cryptoRowArray[3] = (amountOwned * jsonData.RAW[cryptoID].USD.PRICE).toFixed(2);
+			var pricePerCoin = (jsonData.RAW[cryptoID].USD.PRICE).toFixed(2);	
+			var cryptoOwned = amountOwned.toFixed(2);
+			var totalInvested = (amountOwned * jsonData.RAW[cryptoID].USD.PRICE).toFixed(2);
 
-			var row = document.getElementById("row" + index);
-			for(var i = 0; i < 4; i++){
-				row.cells[i].innerHTML = cryptoRowArray[i];
-			}
-			index++;
+			tableText += "<tr class=\"coins\"><td>" + cryptoID + "</td><td>" + pricePerCoin + "</td><td>" + cryptoOwned + "</td><td>" + totalInvested + "</td>";
 		}
+		tableText += "</table>";
+      	document.getElementById("crypto_table_container").innerHTML = tableText;
+
+      	displayGraphAndDetailsOnRowClick();
 	}};
 	xml.send(null);
 }
