9ef622f9b555bec14b0efbb1a9936d866d55391a
Sam Gura <sggura@buffalo.edu>
2018-12-03 09:50:14 -0500

    set search to return top 5 results w/ info

--- stock_market.js
+++ stock_market.js
@@ -8,14 +8,16 @@ function clicked(){
 	getStock.onreadystatechange = function(){
 		if (getStock.readyState === 4 && getStock.status === 200){
 			var jsonreply = JSON.parse(getStock.responseText)
-			injectTxt += "<table><thead><tr class=table100-head><th class=column1>Name</th><th class=column2>Price</th><th class=column3>Daily High</th><th class=column4>Daily Low</th><th class=column5>24h change</th><th class=column6> Volume</th></tr><tbody>";
+			injectTxt += "<table><thead><tr class=table100-head><th class=column1> Name </th><th class=column2> Symbol </th><th class=column2> Price </th><th class=column2> Daily High </th><th class=column2> Daily Low </th><th class=column2> 24h change </th><th class=column2> Volume </th><th class=column6> Buy/Sell </th></tr><tbody>";
 			var sym = jsonreply.quote.symbol;
+			var name = jsonreply.quote.companyName;
 			var price = jsonreply.quote.latestPrice;
 			var dayHigh = jsonreply.quote.high;
 			var dayLow = jsonreply.quote.low;
 			var dayChange = jsonreply.quote.change;
-			var vol = jsonreply.quote.avgTotalVolume;
-			injectTxt += "<tr><td class=column1>" + sym +"</td><td class=column2> $" + price + "</td><td class=column3>$" + dayHigh + "</td><td class=column4>$" + dayLow + "</td><td class=column5>" + dayChange + "%</td><td class=column6>" + vol + "</td></tr>";
+			var vol = jsonreply.quote.latestVolume;
+			var newVol = vol.toLocaleString('en');
+			injectTxt += "<tr><td class=column1>" + name +"</td><td class=column2>"+ sym +"</td><td class=column2> $" + price + "</td><td class=column2>$" + dayHigh + "</td><td class=column2>$" + dayLow + "</td><td class=column2>" + dayChange + "%</td><td class=column2>" + newVol + "</td><td class=column6><form><input type=text></input></form><button type=button> Buy </button><button type=button> Sell </button></td></tr>";
 			injectTxt += "</tbody></table>";
 			document.getElementById("stock_results").innerHTML = injectTxt;
 		}
